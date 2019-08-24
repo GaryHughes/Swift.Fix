f7cd2d9348cecc9a0143429bc3b9743b64442534
Gary Hughes <garyedwardhughes@gmail.com>
2019-08-24 12:42:54 +1000

    Update to build with Xcode 10

--- Fix/FixTests/FixTests.swift
+++ Fix/FixTests/FixTests.swift
@@ -28,7 +28,7 @@ class FixTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
