a26eb4ae957a4cff09c92a46ad4eda9421185b8a
Gary Hughes <garyedwardhughes@gmail.com>
2021-03-13 16:58:00 +1100

    Basic field definition.

---
+++ Fix/Fix/Field.swift
@@ -0,0 +1,71 @@
+//
+//  Field.swift
+//  Fix
+//
+//  Created by Gary Hughes on 13/3/21.
+//  Copyright © 2021 Gary Hughes. All rights reserved.
+//
+import Foundation
+
+public struct Field
+{
+    public let tag: Int
+    public let value: String
+    
+    public init(tag: Int, value: Bool)
+    {
+        self.tag = tag
+        self.value = value ? "Y" : "N"
+    }
+    
+    public init(tag: Int, value: Int8)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: Int16)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: Int32)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: Int64)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: UInt8)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: UInt16)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: UInt32)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+    public init(tag: Int, value: UInt64)
+    {
+        self.tag = tag
+        self.value = value.description
+    }
+    
+}
+
+
---
+++ Fix/FixTests/FieldTests.swift
@@ -0,0 +1,50 @@
+//
+//  FieldTests.swift
+//  FieldTests
+//
+//  Created by Gary Hughes on 10/10/2014.
+//  Copyright (c) 2014 Gary Hughes. All rights reserved.
+//
+import Cocoa
+import XCTest
+import Fix
+
+class FieldTests: XCTestCase {
+    
+    func testBooleanInitialisers()
+    {
+        XCTAssertEqual(Field(tag:0, value:true).value, "Y")
+        XCTAssertEqual(Field(tag:0, value:false).value, "N")
+    }
+    
+    func testNumericInitialisers()
+    {
+        XCTAssertEqual(Field(tag:0, value:Int8.min).value, "-128")
+        XCTAssertEqual(Field(tag:0, value:Int8.max).value, "127")
+        
+        XCTAssertEqual(Field(tag:0, value:Int16.min).value, "-32768")
+        XCTAssertEqual(Field(tag:0, value:Int16.max).value, "32767")
+        
+        XCTAssertEqual(Field(tag:0, value:Int32.min).value, "-2147483648")
+        XCTAssertEqual(Field(tag:0, value:Int32.max).value, "2147483647")
+        
+        XCTAssertEqual(Field(tag:0, value:Int64.min).value, "-9223372036854775808")
+        XCTAssertEqual(Field(tag:0, value:Int64.max).value, "9223372036854775807")
+        
+        XCTAssertEqual(Field(tag:0, value:UInt8.min).value, "0")
+        XCTAssertEqual(Field(tag:0, value:UInt8.max).value, "255")
+        
+        XCTAssertEqual(Field(tag:0, value:UInt16.min).value, "0")
+        XCTAssertEqual(Field(tag:0, value:UInt16.max).value, "65535")
+        
+        XCTAssertEqual(Field(tag:0, value:UInt32.min).value, "0")
+        XCTAssertEqual(Field(tag:0, value:UInt32.max).value, "4294967295")
+        
+        XCTAssertEqual(Field(tag:0, value:UInt64.min).value, "0")
+        XCTAssertEqual(Field(tag:0, value:UInt64.max).value, "18446744073709551615")
+    }
+    
+    
+    
+    
+}
